a0b9df42cedb482e217ace05e83783c8be5fd04e
Aaron Snow <ain2108@raspberrypi.(none)>
2016-03-20 20:38:03 +0000

    Working on makins sure that client works as expected.

--- server/src/handle_client.c
+++ server/src/handle_client.c
@@ -2,6 +2,23 @@
 
 void handle_client(int sock){
 
+  // Creating FILE pointers for convenience
+  FILE * sockf_write = fdopen(sock, "w");
+  FILE * sockf_read = fdopen(sock, "r");
+  char line[2048];
+  memset(line, 0, 2048);
+  
+  // Login
+  readLine(sockf_read, line, 2048 - 1);
+  fprintf(sockf_write, "OK\n");
+  fprintf(sockf_write, "Welcome user!\n");
+
+  // Echo
+  int charsRead = 0;
+  while( (charsRead = readLine(sockf_read, line, 2048 - 1)) >= 0){
+    fprintf(sockf_write, "%s\n", line);
+  }
+
 
   close(sock);
   return;
