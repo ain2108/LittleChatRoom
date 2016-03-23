4dd2fdee387651b6ee71967b3f7f6c0ec4e80132
Anton Nefedenkov <ain2108@columbia.edu>
2016-03-23 15:41:15 -0400

    Debugging like crazy

--- server/src/interpreter.c
+++ server/src/interpreter.c
@@ -12,8 +12,9 @@ int interpret(int socket, char * read_line, UsersDBRec * users_rec){
 
   // Get the comman from the line
   char * token;
+  read_line[strlen(read_line)] = ' ';
   token = strtok(read_line, " ");
-
+  
   // 'switch' on the token
   if(!strcmp(token, COMMAND_WHO)){
     perform_WHO(socket, &users_db);
@@ -34,6 +35,20 @@ int interpret(int socket, char * read_line, UsersDBRec * users_rec){
     perform_BROADCAST(token, users_rec, &users_db);
     return 0;
   }else if(!strcmp(token, COMMAND_SEND)){
+    char * receiver_msg = strtok(NULL, "\0");
+    // If receiver was not provided
+    if(receiver_msg == NULL){
+      send_to_client(socket, "server: ");
+      send_to_client(socket, "please provide destination\n");   
+      return 0;
+    }
+    // If its a multicase message
+    if( ((*receiver_msg) == '(') && strstr(receiver_msg, ")") != NULL){
+      (*receiver_msg) = ' ';
+      perform_MULTI_SEND(receiver_msg, &users_db, socket);
+      return 0;
+    }
+    perform_SEND(receiver_msg, &users_db, socket, users_rec);
     return 0;
   }else if(!strcmp(token, COMMAND_LOGOUT)){
     send_to_client(socket, "server: logging out...\n");
@@ -120,8 +135,51 @@ void perform_BROADCAST(char * message, UsersDBRec * users_rec, UsersDB * db){
   return;
 }
 
-void perform_SEND(){
+void perform_SEND(char * receiver_message, UsersDB * db, 
+		  int socket, UsersDBRec * rec){
+  char * token = strtok(receiver_message, " ");
+  if(token == NULL){
+    send_to_client(socket, "server: ");
+    send_to_client(socket, "please provide destination\n");   
+    return;
+  }
+  if(!check_username_exits_and_online(token, db)){
+    send_to_client(socket, "server: ");
+    send_to_client(socket, token);
+    send_to_client(socket, " offline or does not exits\n");
+    return;
+  }
+  
+  // Extract username
+  char username[USERNAME_LIMIT];
+  memset(username, '\0', USERNAME_LIMIT);
+  strcpy(username, token);
+  token = strtok(NULL, "\0");
+  if( (*token) == '\0') return;
 
+  send_to_receivers_pipe(username, rec->login);
+  send_to_receivers_pipe(username, ": ");
+  send_to_receivers_pipe(username, token);
+  send_to_receivers_pipe(username, "\n");
+
+  return;
+}
+
+int check_username_exits_and_online(char * login, UsersDB * db){
+
+  // Iterate through database, check stuff.
+  int i;
+  for(i = 0; i < N_USERS; i++){
+    if( !strcmp(db->records[i].login, login) && db->records[i].logged_in){
+      return 1;
+    }
+  }
+  return 0; // Either does not exist or offline
+}
+
+void perform_MULTI_SEND(char * receivers_message, UsersDB * db, int socket){
+  fprintf(stderr, "multisend\n");
+  return;
 }
 
 
