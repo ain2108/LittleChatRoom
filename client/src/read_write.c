5380901d48d225f95c3243442d5d66d37c9ee67a
Anton Nefedenkov <ain2108@columbia.edu>
2016-03-23 19:38:40 -0400

    cleaned up a little

--- client/src/read_write.c
+++ client/src/read_write.c
@@ -1,35 +1,5 @@
 #include "read_write.h"
 
-// This function never returns. Make sure its run in separate proccess.
-void f_continous_reading(FILE * from, FILE * to){
-
-  char buffer[READ_BUFFER];
-  while(1){
-    memset(buffer, 0, READ_BUFFER); // Not to bother with '\0'
-    fread(buffer, 1, READ_BUFFER - 1, from); // -1 for '\0'
-    fwrite(buffer, 1, strlen(buffer) + 1, to);
-  }
-
-  return; // NEVER REACHED
-}
-
-
-void s_continous_reading(int from, int to){
-
-  char buffer[READ_BUFFER];
-  int bytesReceived;
-  FILE * toFile = fdopen(to, "w");
-  memset(buffer, 0, READ_BUFFER); // Not to bother with '\0'
-
-  while((bytesReceived = recv(from, buffer, READ_BUFFER - 1, 0)) > 0){
-
-    fwrite(buffer, 1, READ_BUFFER, toFile);
-    memset(buffer, 0, READ_BUFFER); // Not to bother with '\0'
-  }
-
-  return; // NEVER REACHED
-}
-
 // Reads a line into buffer, no \n char 
 int readLine(FILE * sockf, char * buffer, size_t buffSize){
   memset(buffer, 0, buffSize);
--- client/src/test.c
+++
@@ -1,9 +0,0 @@
-#include "login.h"
-
-int main1(){
-
-  //FILE * testf = fopen("test.txt", "w");
-  //send_login(testf);
-
-  return 0;
-}
